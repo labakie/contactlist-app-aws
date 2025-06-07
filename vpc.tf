7581c0ccd74bcc87804a1dce54f994d64e0eb17d
labakie <95135910+labakie@users.noreply.github.com>
2025-06-07 15:54:33 +0700

    Create VPC configuration.

---
+++ vpc.tf
@@ -0,0 +1,64 @@
+# create VPC
+resource "aws_vpc" "custom_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "ContactList_VPC"
+  }
+}
+
+# create two public subnets
+resource "aws_subnet" "public" {
+  # using meta-argument count
+  count = length(var.public_subnet_cidr)
+  vpc_id = aws_vpc.custom_vpc.id
+  cidr_block = element(var.public_subnet_cidr, count.index)
+  availability_zone = (var.zones, count.index)
+
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+ }
+}
+
+# create two private subnets
+resource "aws_subnet" "private" {
+  # using meta-argument count
+  count = length(var.private_subnet_cidr)
+  vpc_id = aws_vpc.custom_vpc.id
+  cidr_block = element(var.private_subnet_cidr, count.index)
+  availability_zone = (var.zones, count.index)
+
+  tags = {
+    Name = "Private Subnet ${count.index + 1}"
+ }
+}
+
+# create internet gateway
+resource "aws_internet_gateway" "custom_igw" {
+  vpc_id = aws_vpc.custom_vpc.id
+  
+  tags = {
+    Name = "ContactList_igw"
+  }
+}
+
+# create public route table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.custom_igw.id
+  }
+
+  tags = {
+    Name = "public route table"
+  }
+}
+
+# associate public subnets with the route table
+resource "aws_route_table_association" "public_associate" {
+  count = length(var.public_subnet_cidr)
+  subnet_id = element(var.public_subnet_cidr, count.index)
+  route_table_id = aws_route_table.public_rt.id
+}
