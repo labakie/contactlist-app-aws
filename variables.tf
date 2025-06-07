acd65a3b686a7f06ec46cb4a560947a129b436b0
labakie <95135910+labakie@users.noreply.github.com>
2025-06-07 16:50:01 +0700

    Create varables for VPC.

--- variables.tf
+++ variables.tf
@@ -1,25 +1,33 @@
-# variable "PolicyName" {
-#   default = "CustomLambdaToDynamoDBPolicy"
-# }
+variable "public_subnet_cidr" {
+  type = list(string)
+  default = [ "10.0.1.0/24", "10.0.2.0/24" ]
+}
 
-# variable "PolicyDescription" {
-#   default = "Policy for access dynamodb and enable log from function."
-# }
+variable "private_subnet_cidr" {
+  type = list(string)
+  default = [ "10.0.3.0/24", "10.0.3.0/24" ]
+}
 
-# variable "RoleName" {
-#   default = "CustomLambdaToDynamoDBRole"
-# }
+variable "us_zones" {
+  type = list(string)
+  default = [ "us-east-1a", "us-east-1b" ]
+}
 
-# variable "RoleDescription" {
-#   default = "Role for Lambda."
-# }
+variable "id_zones" {
+  type = list(string)
+  default = [ "ap-southeast-3a", "ap-southeast-3b" ]
+}
 
-# variable "LambdaName" {
-#   default = "IdentityFunction"
-# }
+variable "instance_type" {
+  type = map(string)
+  default = {
+    "us" = "t2.micro"
+    "id" = "t3.micro"
+  }
+}
 
 variable "DynamoDBName" {
-  default = "UserIdentityTable"
+  default = "ContactListTable"
 }
 
 # FOR IAM.tf
