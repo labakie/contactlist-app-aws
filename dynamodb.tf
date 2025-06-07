a204cc5b833567d19ca21503479e9694010f6e85
labakie <95135910+labakie@users.noreply.github.com>
2025-06-07 16:40:13 +0700

    Edit resource name.

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,5 +1,5 @@
 # create dynamodb table
-resource "aws_dynamodb_table" "user_identity" {
+resource "aws_dynamodb_table" "contact_list" {
   name         = var.DynamoDBName
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "email"
